ce3f0e18e349fc79f95d8f6056d0c925e2e2d673
QwisoDev <>
2018-10-03 15:53:08 -0400

    use className not class

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const Content = (props) => {
-  return <h3>some content</h3>
+  return <h3>content renders here</h3>
 }
 
 class App extends Component {
   render() {
     return (
       <div>
-        <section>
-        <div class="container-fluid">
+        <section name="content">
+        <div className="container-fluid">
           <Content />
         </div>
         </section>
-        <section>
-          <div class="container-fluid fixed-bottom bg-warning">
-            <div class="row d-flex justify-content-around">
-            <h3 class="p-3 m-0 text-white border"><i class="fas fa-users"></i></h3>
-              <h3 class="p-3 m-0 text-white border"><i class="fas fa-map-marked"></i></h3>
-              <h3 class="p-3 m-0 text-white border"><i class="fas fa-search"></i></h3>
+        <section name="nav-bar">
+          <div className="container-fluid fixed-bottom bg-warning">
+            <div className="row d-flex justify-content-around">
+              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-users"></i></h3>
+              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-map-marked"></i></h3>
+              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-search"></i></h3>
             </div>
           </div>
         </section>
---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div id="map">
+                
+            </div>
+        )
+    }
+}
+ 
+export default Map;
