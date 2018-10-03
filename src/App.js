5f90d413d6d5f00d7962fbf9ce065fa197ef6e26
QwisoDev <>
2018-10-03 15:38:51 -0400

    mocking up ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+const Content = (props) => {
+  return <h3>some content</h3>
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div claassName="row">
-          <div className="col">
-            <div className="text-center bg-secondary">
-              <h3>Hello world</h3>
+      <div>
+        <section>
+        <div class="container-fluid">
+          <Content />
+        </div>
+        </section>
+        <section>
+          <div class="container-fluid fixed-bottom bg-warning">
+            <div class="row d-flex justify-content-around">
+            <h3 class="p-3 m-0 text-white border"><i class="fas fa-users"></i></h3>
+              <h3 class="p-3 m-0 text-white border"><i class="fas fa-map-marked"></i></h3>
+              <h3 class="p-3 m-0 text-white border"><i class="fas fa-search"></i></h3>
             </div>
           </div>
-        </div>
-      </div>
+        </section>
+      </div> 
     )
   }
 }
