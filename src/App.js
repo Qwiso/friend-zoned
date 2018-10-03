e16ce1b037442288d35e54344116afda421cc7a6
QwisoDev <>
2018-10-03 16:34:57 -0400

    trying to get page navigation to work

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const Content = (props) => {
-  return <h3>content renders here</h3>
+  return <h3>{props.page} renders here</h3>
 }
 
 class App extends Component {
+  state = {
+    page: "main"
+  }
+
+  navBtnClicked = (e) => {
+    console.log(e)
+    if (!this.state.page === e) {
+      this.setState({
+        page: e
+      })
+    }
+  }
+
   render() {
     return (
       <div>
         <section name="content">
         <div className="container-fluid">
-          <Content />
+          <Content page={this.state.page} />
         </div>
         </section>
-        <section name="nav-bar">
-          <div className="container-fluid fixed-bottom bg-warning">
-            <div className="row d-flex justify-content-around">
-              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-users"></i></h3>
-              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-map-marked"></i></h3>
-              <h3 className="btn btn-lg p-3 m-0 text-white"><i className="fas fa-search"></i></h3>
+        <section name="nav-bar" className="d-block">
+          <div className="container-fluid fixed-bottom bg-secondary">
+            <div className="row">
+              <div onClick={this.navBtnClicked('main')} className="btn btn-lg col py-2 border-y rounded-0 text-center text-info"><i className="fas fa-lg fa-map-marked"></i></div>
+              <div onClick={this.navBtnClicked('map')} className="btn btn-lg col py-2 border-y rounded-0 text-center text-white"><i className="fas fa-lg fa-users"></i></div>
+              <div onClick={this.navBtnClicked('search')} className="btn btn-lg col py-2 border-y rounded-0 text-center text-white"><i className="fas fa-lg fa-search"></i></div>
             </div>
           </div>
         </section>
