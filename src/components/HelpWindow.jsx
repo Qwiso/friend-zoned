7c060dcfcd530fc268a18038509d983455c83746
QwisoDev <>
2018-10-10 15:58:12 -0400

    modal window

--- src/components/HelpWindow.jsx
+++ src/components/HelpWindow.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import '../css/helpwindow.css'
 
 export class HelpWindow extends Component {
     state = {
@@ -9,9 +8,19 @@ export class HelpWindow extends Component {
     render() { 
         return (
             <section name="helpwindow">
-                <div className="position-absolute container-fluid bg-fade">
-                    <div className="row bg-white">
+                <div style={{backgroundColor: "rgba(0, 0, 0, 0.33)"}} className="position-absolute w-100 h-100 d-flex justify-content-center">
+                    <div className="col-6 h-50 my-auto bg-white rounded">
+                        <div className="row">
+                            <div class="col">
+                                <h3 className="text-center">help window</h3>
+                            </div>
+                        </div>
 
+                        <div className="row">
+                            <div class="col">
+                                blah blah blah
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
