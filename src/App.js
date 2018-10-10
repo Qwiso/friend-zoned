63f7bc6caa3803e6cd352945f7e468a5efb2a6d7
QwisoDev <>
2018-10-10 09:14:21 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route, Redirect, NavLink } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-import { Main } from "./components/Main"
 import MapView from "./components/MapView"
-import { User } from "./components/User"
-import { Explore } from "./components/Explore"
+// import { User } from "./components/User"
+// import { Explore } from "./components/Explore"
 
 const checkAuth = () => {
-  // TODO ping server to check or refresh user stuff
   return true
 }
 
@@ -28,21 +26,13 @@ class App extends Component {
         <div>
           <section name="content">
             <div className="container-fluid p-0">
-            <Switch>
+              <AuthRoute exact path="/" component={MapView} />
+            {/* <Switch>
               <AuthRoute exact path="/" component={Main} />
               <AuthRoute path="/map" component={MapView} />
               <AuthRoute path="/user" component={User} />
               <AuthRoute path="/explore" component={Explore} />
-            </Switch>
-            </div>
-          </section>
-          <section name="navbar" className="d-block">
-            <div className="container-fluid fixed-bottom bg-dark" style={{height: "46px"}}>
-              <div className="row d-flex justify-content-around text-center">
-                <NavLink to="/map" activeClassName="bg-info" className="col"><i className="fas fa-lg fa-map-marked p-3 text-white"></i></NavLink>
-                <NavLink to="/user" activeClassName="bg-info" className="col"><i className="fas fa-lg fa-users p-3 text-white"></i></NavLink>
-                <NavLink to="/explore" activeClassName="bg-info" className="col"><i className="fas fa-lg fa-search p-3 text-white"></i></NavLink>
-              </div>
+            </Switch> */}
             </div>
           </section>
         </div>
--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -9,13 +9,14 @@ const icon = {
     scale: 0.075,
     strokeColor: '#000',
     strokeWeight: 1,
-    fillColor: '#111',
-    fillOpacity: 0.5,
+    fillColor: '#fff',
+    fillOpacity: 0.66,
     anchor: { x: foo.icon[0]/2, y: foo.icon[1] }
 }
 
 class MapView extends Component {
     state = {
+        mapStyle: testing,
         markers: [],
         showingInfoWindow: false,
         activeMarker: {},
@@ -23,6 +24,7 @@ class MapView extends Component {
     }
 
     onMarkerClick = (a, b, e) => {
+        // a: icon, name, position
         this.setState({
             selectedPlace: a,
             activeMarker: b,
@@ -44,30 +46,41 @@ class MapView extends Component {
     }
 
     render() {
-                
         let markerRender = this.state.markers.map((marker, index) => {
             return <Marker draggable={true} name={index} onClick={this.onMarkerClick} key={index} position={marker} animation={window.google.maps.Animation.DROP} icon={icon}/>
         })
 
         return (
-            <Map
-                styles={testing}
-                onClick={this.onMapClicked}
-                google={this.props.google} 
-                zoom={8}
-                initialCenter={{
-                    lat: 33.83008972168741,
-                    lng: -84.35267661111448 
-                }}>
+            <div>
+                <Map
+                    style={{marginBottom: 46}}
+                    styles={this.state.mapStyle}
+                    onClick={this.onMapClicked}
+                    google={this.props.google} 
+                    zoom={10}
+                    initialCenter={{
+                        lat: 33.83008972168741,
+                        lng: -84.35267661111448 
+                    }}>
 
-                {markerRender}
+                    {markerRender}
 
-                <InfoWindow 
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}>
-                    <h1>{this.state.selectedPlace.name}</h1>
-                </InfoWindow>
-            </Map>
+                    <InfoWindow 
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </InfoWindow>
+                </Map>
+                <section name="actionBar" className="d-block">
+                    <div className="container-fluid fixed-bottom bg-dark" style={{height: "46px"}}>
+                        <div className="row d-flex justify-content-around text-center">
+                            <div onClick={this.toggleLeftSidebar} activeclass="bg-info" className="col"><i className="fas fa-lg fa-map-marked p-3 text-white"></i></div>
+                            <div onClick={this.toggleMarkerPlacement} activeclass="bg-info" className="col"><i className="fas fa-lg fa-users p-3 text-white"></i></div>
+                            <div onClick={this.toggleRightSidebar} activeclass="bg-info" className="col"><i className="fas fa-lg fa-search p-3 text-white"></i></div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         )
     }
 }
