ad1eae1dc5d990609a53722ab69d9a8d5d7b6de5
QwisoDev <>
2018-10-17 12:10:37 -0400

    build project

---
+++ build/precache-manifest.d0454996e61a2b909921ec0f07ec6adf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "263f4728d202107a650f",
+    "url": "/static/css/main.8ec4f04b.chunk.css"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "bef688aff391a68aab69",
+    "url": "/static/js/1.bef688af.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "263f4728d202107a650f",
+    "url": "/static/js/main.263f4728.chunk.js"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "bef688aff391a68aab69",
+    "url": "/static/css/1.a446698f.chunk.css"
+  },
+  {
+    "revision": "388b7d9e2beb0ddd9ca1b51478e5b30c",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/friend-zoned/precache-manifest.85ad8728f0f6f738346952569f045d02.js"
+  "/precache-manifest.d0454996e61a2b909921ec0f07ec6adf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/friend-zoned/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
