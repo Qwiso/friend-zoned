72fec56526a2df90d651d995198aa7feae2ec512
QwisoDev <>
2018-10-31 14:12:34 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import firebase from 'firebase'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
+import withAuthentication from './components/auth/withAuthentication'
+
 import MapView from './components/map/MapView'
-// import Login from './components/auth/Login'
+import Login from './components/auth/Login'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,12 +19,25 @@ const firebaseConfig = {
 }
 firebase.initializeApp(firebaseConfig)
 
+const Logout = () => {
+  firebase.auth().signOut()
+  return <Redirect to='/login' />
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      authUser: null
+    }
+  }
+  
   componentDidMount() {
-    firebase.auth().onAuthStateChanged((user) => {
-      this.setState({
-        user: user
-      })
+    firebase.auth().onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null })
     })
   }
 
@@ -35,6 +50,8 @@ class App extends Component {
                 <Route exact path="/" component={MapView} />
                 <Route exact path="/map/:id" component={MapView} />
                 <Route exact path="/:id/with/:others" component={MapView} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/logout" component={Logout} />
               </Switch>
             </div>
           </section>
@@ -43,4 +60,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default withAuthentication(App)
