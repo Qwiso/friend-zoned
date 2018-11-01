6e90aa23e84b76c09baa59ea8623227129cc5271
QwisoDev <>
2018-11-01 11:01:23 -0400

    removing unused code

--- src/App.js
+++ src/App.js
@@ -25,22 +25,6 @@ const Logout = () => {
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      authUser: null
-    }
-  }
-  
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null })
-    })
-  }
-
   render() {
     return (
       <BrowserRouter>
